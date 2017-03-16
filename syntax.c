0ecd62a1ef57c45895dd1cab6a1a89d21cd13085
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2017-03-16 10:13:57 -0400

    Printing Hello World.

---
+++ syntax.c
@@ -0,0 +1,15 @@
+/*
+Make a single program in the C language by augmenting and integrating the example C code in Section 4.2
+and 4.4.1 on the textbook. The program is supposed to read a text file line by line. For each line, it produces
+the output similar to what's shown on Page 178 and 179. If a syntax error is found, it should print an error
+message indicating where the error occurs in the line.
+The program should be invoked by the following command:
+parser <file_name>
+Where <file_name> should be replaced by a real file name.
+*/
+
+#include <stdio.h>
+int main(){
+	printf("Hello");
+	return 0;
+}
